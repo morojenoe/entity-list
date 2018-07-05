729871eab7ff1f9865d64fa957c113b1936be68e
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2018-07-05 09:18:12 +0400

    added material library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { EntityListComponent } from './components/entity-list/entity-list.component';
@@ -12,7 +13,8 @@ import { EntityListComponent } from './components/entity-list/entity-list.compon
     EntityListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
